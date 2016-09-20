c410520dc7b8cf59997bf8714be0ae8583c97202
Pauline Ndiaye <Ayshoox@macbook-pro-de-pauline.home>
2016-09-20 12:31:01 +0200

    index.php 22

--- index.php
+++ index.php
@@ -1,1 +1,177 @@
-<h1>TRY</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Test Email 2</title>
+</head>
+<body>
+
+  <div style="background:#000;">
+
+  <body style="padding:0; margin:0" bgcolor="#687079">
+
+  <table border="0" cellpadding="0" cellspacing="0" style="margin: 0; padding: 0" width="100%">
+      <tr>
+          <td align="center" valign="top">
+
+              <table width="640" border="0" cellspacing="0" cellpadding="0" class="hide">
+                  <tr>
+                      <td height="20"></td>
+                  </tr>
+              </table>
+
+              <table width="640" cellspacing="0" cellpadding="0" bgcolor="#" class="100p">
+                  <tr>
+                      <td background="images/header-bg.jpg" bgcolor="#fff" width="640" valign="top" class="100p">
+                                  <div>
+                                      <table width="640" border="0" cellspacing="0" cellpadding="20" class="100p" bgcolor="#f4f4f4">
+                                          <tr>
+                                              <td valign="top">
+                                                  <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p" >
+
+
+                                                      <tr>
+                                                          <td align="left" width="40%" class="100p"><img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/visa2.png" alt="Logo" border="0" style="display:block" /></td>
+                                                          <td width="60%" class="hide" align="right" style="font-size:16px; color:rgb(139, 147, 158);"><font face="'Roboto', Arial, sans-serif"><a href="#" style="color:rgb(139, 147, 158); text-decoration:none;">Features</a> &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp; <a href="#" style="color:rgb(139, 147, 158); text-decoration:none;">Pricing</a> &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp; <a href="#" style="color:rgb(139, 147, 158); text-decoration:none;">Subscribe</a></font></td>
+                                                      </tr>
+                                                  </table>
+                                                  <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p">
+                                                      <tr>
+                                                          <td height="35"></td>
+                                                      </tr>
+                                                      <tr>
+                                                          <td align="center" style="color:#FFFFFF; font-size:24px;">
+                                                              <font face="'Roboto', Arial, sans-serif">
+                                                                  <span style="font-size:33px;color:rgb(94, 100, 109);">Showcase your awesome product</span><br />
+                                                                  <br />
+                                                                  <Span style="font-size:20px;color:rgb(139, 147, 158);">With Visa's awesome template</Span>
+                                                                  <br /><br />
+
+                                                                  <a href="##" style="color:#FFFFFF; text-decoration:none;">
+                                                                  <table border="0" cellspacing="0" cellpadding="10" >
+                                                                      <tr>
+                                                                          <td align="center" style="color:#FFFFFF; font-size:16px;"><font face="'Roboto', Arial, sans-serif"><a href="##" style="border: 1px solid rgb(247, 228, 150);color: #fff;background-color: rgb(247, 228, 150);padding: 10px 30px;border-radius: 18px;font-weight: bold;cursor: pointer;text-decoration:none">Try Now</a></font></td>
+                                                                      </tr>
+                                                                  </table>
+                                                                  </a>
+                                                              </font>
+                                                          </td>
+                                                      </tr>
+                                                      <tr>
+                                                          <td height="35"></td>
+                                                      </tr>
+                                                      <td valign="top" align="center" class="100padleftright">
+                                                          <table border="0" cellspacing="0" cellpadding="0">
+                                                              <tr>
+                                                                  <td width="135" align="center"><a href="#"><img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/creditcard.png" border="0" style="display:block;height:200px;width:300px;"/></a></td>
+                                                                  <td width="20"></td>
+                                                              </tr>
+                                                          </table>
+                                                      </td>
+                                                  </table>
+                                              </td>
+                                          </tr>
+                                          <td valign="top">
+                                              <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p" bgcolor="#fff">
+                                                  <tr>
+                                                      <td height="60"></td>
+                                                  </tr>
+                                                  <tr>
+                                                      <td align="center" style="color:#FFFFFF; font-size:24px;">
+                                                          <font face="'Roboto', Arial, sans-serif">
+                                                              <span style="font-size:33px;color: rgb(94, 100, 109);">Features</span><br />
+                                                              <br />
+                                                              <Span style="font-size:20px;color:rgb(139, 147, 158);">Discover the futur</Span>
+                                                              <br /><br />
+
+                                                              <a href="##" style="color:#FFFFFF; text-decoration:none;">
+
+                                                              </a>
+                                                              <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p" >
+                                                                  <tr>
+                                                                    <td align="left" width="33%" class="100p" style="color:rgb(94, 100, 109);text-align:center;font-size:17px" > Cloud Upload</td>
+                                                                    <td align="left" width="33%" class="100p" style="color:rgb(94, 100, 109);text-align:center;font-size:17px" >Easy to use</td>
+                                                                    <td align="left" width="33%" class="100p" style="color:rgb(94, 100, 109);text-align:center;font-size:17px" >Ecommerce</td>
+                                                                  </tr>
+                                                              </table>
+                                                              <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p" >
+                                                                  <tr>
+                                                                    <td align="left" width="33%" class="100p" style="color:rgb(94, 100, 109);text-align:center;font-size:17px" > CloLorem ipsum dolor sit amet, consectetur adipisicing elit</td>
+                                                                    <td align="left" width="33%" class="100p" style="color:rgb(94, 100, 109);text-align:center;font-size:17px" >EaLorem ipsum dolor sit amet, consectetur adipisicing elit</td>
+                                                                    <td align="left" width="33%" class="100p" style="color:rgb(94, 100, 109);text-align:center;font-size:17px" >Lorem ipsum dolor sit amet, consectetur adipisicing elit</td>
+                                                                  </tr>
+                                                                  <td height="80"></td>
+
+                                                          </font>
+                                                      </td>
+
+                                                  </tr>
+
+                                                  <tr>
+                                                      <td height="35"></td>
+                                                  </tr>
+                                                  <td valign="top" align="center" class="100padleftright">
+                                                  </td>
+                                              </table>
+
+                                          </td>
+                                      </table>
+                                      <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p">
+                                          <tr>
+                                              <td height="80"></td>
+                                              <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p" >
+                                                  <tr>
+                                                    <td width="135" align="center"><a href="#"><img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/creditcard.png" border="0" style="display:block;height:200px;width:300px;"/></a></td>
+                                                    <td width="60%" class="hide"  style="font-size:25px; color:rgb(139, 147, 158);text-decoration:none;"><font face="'Roboto', Arial, sans-serif"><span style="color: rgb(94, 100, 109);">The unsee blade is the deadlist</span> </br><span style="font-size:16px;text-align:left;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. cati unde voluptatibus optio distinctio nulla totam maiores aperiam autem facilis asperiores.</span></br> <span><a href="#" style="text-decoration: none;font-size:18px;color: rgb(247, 228, 150);font-weight: bold;">Read more...</a></span></font></td>
+                                                  </tr>
+                                              </table>
+                                          </tr>
+                                          <td height="20"></td>
+                                          <tr>
+
+                              </table>
+                              <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p">
+                                  <tr>
+                                      <td height="80"></td>
+                                      <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p" >
+                                        <td align="center" style="color:#FFFFFF; font-size:24px;">
+                                            <font face="'Roboto', Arial, sans-serif">
+                                                <span style="font-size:20px;color: rgb(247, 228, 150);font-weight:bold;">Gallery</span><br />
+                                                <br />
+                                                <Span style="font-size:20px;color: rgb(94, 100, 109);">The truth lies in darkness.</Span>
+                                                <br /><br />
+
+                                                </tr>
+
+                                    </table>
+                                    <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p" >
+                                      <td align="center" style="color:#FFFFFF; font-size:24px;">
+                                          <font face="'Roboto', Arial, sans-serif">
+                                            <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/cuba.jpeg" alt="Photography of a car" style="width: 150px;height: 100px;">
+                                            <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/djing.jpeg" alt="Djing" style="width: 150px;height: 100px;">
+                                            <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/mountains.jpeg" alt="Photography of a mountain" style="width: 150px;height: 100px;">
+                                            <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/festival.jpeg" alt="Photography of peoples dancing" style="width: 150px;height: 100px;">
+                                            <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/lion.jpeg" alt="Photography of a lion" style="width: 150px;height: 100px;">
+                                            <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/landscape.jpeg" alt="Photography of a landscape" style="width: 150px;height: 100px;">
+                                            <tr>
+                                                <td height="50"></td>
+                                            </tr>
+                                  </table>
+                                  <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p" bgcolor="#f4f4f4" >
+                                    <td align="center" style="color:#FFFFFF; font-size:24px;">
+                                          <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/american-express.png" alt="Amex logo">
+                                          <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/paypal.png" alt="Paypal logo">
+                                          <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/unionpay.png" alt="UnionPay logo">
+                                          <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/mastercard.png" alt="Mastercard logo">
+                                          <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/hsbc.png" alt="HSBC logo">
+                                          <img src="file:///Users/Ayshoox/Desktop/Qonto-Test/qonto-test/images/worldpay.png" alt="WorldPay logo">
+                                    </td>
+                                </table>
+                                <table border="0" cellspacing="0" cellpadding="0" width="600" class="100p" bgcolor="#f4f4f4" >
+                                  <td align="center" style="color:#FFFFFF; font-size:24px;">
+                                    <Span style="font-size:20px;color: rgb(94, 100, 109);">Pauline Ndiaye @Simplon</Span>
+                                  </td>
+                              </table>
+                              </table>
+                                  </div>
+</body>
+</html>
